cd2b02e0ac8ac473eb4373dc8b6822107d65edf5
Tom Okafor <tmeka008@gmail.com>
2025-01-12 23:55:00 +0100

    Configure personal colours theme.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 function App() {
   return (
     <>
-      <h1 className="text-lg text-pink-700 border-2 border-green-400 py-2 px-4 w-fit rounded-lg shadow-lg bg-slate-400 hover:text-white duration-500">AUDIOPHILE</h1>
+      <h1 className="text-pink-dark">AUDIOPHILE</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      pink: {
+        dark: "#ff5d8f",
+        light: "#ffacc5",
+        pale: "#ffcad4",
+      },
+    },
+    fontFamily: {},
+    fontSize: {},
+    spacing: {},
     extend: {},
   },
   plugins: [],
