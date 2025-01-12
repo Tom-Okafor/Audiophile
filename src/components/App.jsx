43da3dce1f838f8ab6671db7a55008a50a4f79bb
Tom Okafor <tmeka008@gmail.com>
2025-01-12 02:01:25 +0100

    Testing, testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 function App() {
   return (
     <>
-      <h1 className="p-8">Hello World!</h1>
+      <h1 className="text-lg text-pink-700 border-2 border-green-400 py-2 px-4 w-fit rounded-lg shadow-lg bg-slate-400 hover:text-white duration-500">AUDIOPHILE</h1>
     </>
   );
 }
